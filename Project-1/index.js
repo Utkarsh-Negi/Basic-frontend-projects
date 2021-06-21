f67c788e4e94df3e9b28ffd588d5faf4120c61e2
Utkarsh <utkarshnegi25@gmail.com>
2021-06-21 14:11:25 +0530

    added more blocks of seats using function in javascript

--- Project-1/index.js
+++ Project-1/index.js
@@ -1,8 +1,16 @@
-const box = document.querySelector('.box');
-box.addEventListener('click', () => {
-  if (box.classList.contains('boxclicked')) {
-    box.classList.remove('boxclicked');
-  } else {
-    box.classList.add('boxclicked');
-  }
+var container = document.querySelector('.front');
+for (i = 0; i < 400; i++) {
+  container.innerHTML += '<div class="box"></div>';
+}
+
+var box = document.querySelectorAll('.box');
+
+box.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    if (item.classList.contains('boxclicked')) {
+      item.classList.remove('boxclicked');
+    } else {
+      item.classList.add('boxclicked');
+    }
+  });
 });
