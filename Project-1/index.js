0bfde2b59819f3ec9b3956bd5f79ba75b48b0b9f
Utkarsh <utkarshnegi25@gmail.com>
2021-06-21 16:12:20 +0530

    added an array in javascript to restore the white blocks

--- Project-1/index.js
+++ Project-1/index.js
@@ -1,9 +1,21 @@
 var container = document.querySelector('.front');
+const defaultWhite = [
+  129, 130, 148, 149, 150, 151, 167, 168, 169, 170, 171, 172, 186, 187, 189,
+  190, 192, 193, 206, 207, 208, 209, 210, 211, 212, 213, 228, 231, 247, 249,
+  250, 252, 266, 268, 271, 273,
+];
 for (i = 0; i < 400; i++) {
   container.innerHTML += '<div class="box"></div>';
 }
 
 var box = document.querySelectorAll('.box');
+for (i = 0; i < 400; i++) {
+  if (defaultWhite.includes(i)) {
+    // Do Nothing
+  } else {
+    box[i].classList.add('boxclicked');
+  }
+}
 
 box.forEach((item) => {
   item.addEventListener('click', (event) => {
